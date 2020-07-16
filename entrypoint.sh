7884f4d6e210155667290b85d1a606ef946404e7
Ankit Gupta <ankit@reverielabs.com>
2020-07-15 23:25:35 -0400

    Basic auto merging functionality (#1)
    * merge
    
    * another one

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,10 +43,22 @@ if [[ "$USER_EMAIL" == "null" ]]; then
 	USER_EMAIL="$USER_LOGIN@users.noreply.github.com"
 fi
 
-if [[ "$(echo "$pr_resp" | jq -r .rebaseable)" != "true" ]]; then
-	echo "GitHub doesn't think that the PR is rebaseable!"
+if [[ "$(echo "$pr_resp" | jq -r .mergeable)" == "false" ]]; then
+	echo "GitHub doesn't think that the PR can be updated automatically!"
 	echo "API response: $pr_resp"
 	exit 1
+fi
+
+if [[ "$(echo "$pr_resp" | jq -r .mergeable)" != "true" ]]; then
+	echo "Mergeablility not computed yet. Trying again in 30 seconds."
+	sleep 30
+	pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
+          "${URI}/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER")
+    if [[ "$(echo "$pr_resp" | jq -r .mergeable)" != "true" ]]; then
+		echo "GitHub doesn't think that the PR can be updated automatically!"
+		echo "API response: $pr_resp"
+		exit 1
+	fi
 fi
 
 if [[ -z "$BASE_BRANCH" ]]; then
@@ -78,7 +90,7 @@ git fetch fork $HEAD_BRANCH
 
 # do the rebase
 git checkout -b $HEAD_BRANCH fork/$HEAD_BRANCH
-git rebase origin/$BASE_BRANCH
+git merge origin/$BASE_BRANCH
 
 # push back
 git push --force-with-lease fork $HEAD_BRANCH
